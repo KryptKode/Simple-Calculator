c255aaf237ffbc9ec3ccb5525fc881d222da2e0f
KryptKode <paulakhamiogu@gmail.com>
2017-08-28 06:08:02 -0800

    Add project files.

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+       private Operations.Operation currentOperation;
+        private double firstNum;
+        private double secondNum;
+        private string input;
+        private string content;
+        private double ans;
+        private bool end;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void BtnEntry_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            DisplayValue(button.Content.ToString());
+                
+        }
+
+        private void BtnDivide_Click(object sender, RoutedEventArgs e)
+        {
+
+            SetOperation(Operations.Operation.Divide, sender);
+              
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            SetOperation(Operations.Operation.Add, sender);
+        }
+
+        private void BtnMultiply_Click(object sender, RoutedEventArgs e)
+        {
+            SetOperation(Operations.Operation.Multiply, sender);
+        }
+
+        private void BtnSubtract_Click(object sender, RoutedEventArgs e)
+        {
+            SetOperation(Operations.Operation.Subtract, sender);
+        }
+
+        private void BtnEquals_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Double.TryParse(input, out secondNum))
+            {
+                SolutionTextBlock.Text = "Error";
+            }
+            else
+            {
+                input = "";
+            }
+
+
+
+           ans =  Operations.PerformOperation(firstNum, secondNum, currentOperation);
+
+            DisplayValue(firstNum + " " +  content + " " + secondNum);
+            SolutionTextBlock.Text = "Ans = " + ans;
+
+            ResetValues();
+        }
+
+        private void ResetValues()
+        {
+            input = "";
+            
+        }
+
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+            ClearValuesTextBox();
+            ClearSolutionTextBox();
+        }
+
+        private void ClearValuesTextBox() {
+            ValuesTextBox.Text = "";
+        }
+
+        private void ClearSolutionTextBox()
+        {
+            SolutionTextBlock.Text = "";
+            input = "";
+        }
+
+        private void DisplayValue (string buttonType) {
+            if (end) {
+                ValuesTextBox.Text = "";
+                end = false;
+            }
+            input += buttonType;
+            ValuesTextBox.Text = input;
+            
+        }
+
+        private void SetOperation(Operations.Operation op, object sender) {
+
+            Button button = (Button)sender;
+            content = button.Content.ToString();
+
+            currentOperation = op;
+            ValuesTextBox.Text = content;
+            if(!Double.TryParse(input, out firstNum)){
+                SolutionTextBlock.Text = "Error";
+            }else {
+                input = "";
+            }
+        }
+    }
+}
---
+++ Calculator/Operations.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Operations
+    {   
+        public enum Operation {Add, Subtract, Multiply, Divide};
+        public static double Add(double num1, double num2) {
+            return num1 + num2;
+        }
+
+        public static double Subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public static double Divide(double num1, double num2)
+        {   
+            if (num1 == 0 || num2 == 0) {
+                return 0; //to avoid division by zero exception
+            }
+            return num1 / num2;
+        }
+
+        public static double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double PerformOperation(double num1, double num2 , Operation operation) {
+            switch (operation) {
+                case Operation.Add:
+                    return Add(num1, num2);
+                case Operation.Subtract:
+                    return Subtract(num1, num2);
+                case Operation.Multiply:
+                    return Multiply(num1, num2);
+                case Operation.Divide:
+                    return Divide(num1, num2);
+                default:
+                    return 0;
+            }
+               
+        }
+
+    }
+}
